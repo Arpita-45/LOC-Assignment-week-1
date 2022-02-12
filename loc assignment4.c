de5eac03bdf342a7a3ef6abc79422caee2d60796
Arpita-45 <99397114+Arpita-45@users.noreply.github.com>
2022-02-12 22:57:11 +0530

    Add files via upload

---
+++ loc assignment4.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch;
+int x,y;
+printf("choose the operator(+,-,*,/,%):");
+scanf("%c",&ch);
+printf("Enter two numbers:");
+scanf("%d%d",&x,&y);
+switch(ch)
+{
+    case'+':
+    printf("%d+%d=\t%d\n",x,y,x+y);
+    break;
+
+    case'-':
+    printf("%d-%d=\t%d\n",x,y,x-y);
+    break;
+
+    case'*':
+    printf("%d*%d=\t%d\n",x,y,x*y);
+    break;
+
+    case'/':
+    printf("%d/%d=\t%d\n",x,y,x/y);
+    break;
+
+    case'%':
+    printf("%d%%d=\t%d\n",x,y,x%y);
+    break;
+    printf("INVALID INPUT!");
+}
+getch();
+}
+
+
